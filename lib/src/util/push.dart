9be57de10e9edf5ecdc1bf5588464ee782f12e49
axinger <liu_weixing@qq.com>
2020-07-12 12:58:46 +0800

    update…

--- lib/src/util/push.dart
+++ lib/src/util/push.dart
@@ -2,8 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// push
-Future<T> push<T>({BuildContext context, Widget widget}) {
-  return Navigator.of(context).push(
+Future<T> push<T>({
+  @required BuildContext context,
+  rootNavigator = true,
+  Widget widget,
+}) {
+  return Navigator.of(context, rootNavigator: rootNavigator).push(
     MaterialPageRoute(builder: (context) {
       return widget;
     }),
@@ -12,13 +16,19 @@ Future<T> push<T>({BuildContext context, Widget widget}) {
 
 /// pop
 /// [result]返回结果
-pop<T>(BuildContext context, [T result]) {
+pop<T>(
+  BuildContext context, [
+  T result,
+]) {
   Navigator.pop(context, result);
 }
 
-///[rootNavigator] 是否隐藏 底部bottomBar
-Future<T> pushCupertino<T>(
-    {BuildContext context, bool rootNavigator = false, Widget widget}) {
+///[rootNavigator] 是否隐藏 底部bottomBar 默认隐藏
+Future<T> pushCupertino<T>({
+  @required BuildContext context,
+  bool rootNavigator = true,
+  Widget widget,
+}) {
   /// 需要配合使用 iOS风格push
   return Navigator.of(context, rootNavigator: rootNavigator).push(
     CupertinoPageRoute(builder: (context) {
