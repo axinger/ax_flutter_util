9af1fb624e8548dad6d19b3647e97688951a6c36
axinger <liu_weixing@qq.com>
2020-07-23 15:43:37 +0800

    update…

--- lib/src/util/push.dart
+++ lib/src/util/push.dart
@@ -14,6 +14,41 @@ Future<T> push<T>({
   );
 }
 
+/// push 跳转并当前页面为根视图
+Future<T> pushAndRemoveUntil<T>({
+  @required BuildContext context,
+  rootNavigator = true,
+  Widget widget,
+}) {
+//  return Navigator.pushAndRemoveUntil(context,
+//      MaterialPageRoute(builder: (context) {
+//        return widget;
+//      }), (route) => route == null);
+
+  return Navigator.of(context, rootNavigator: rootNavigator).pushAndRemoveUntil(
+      MaterialPageRoute(builder: (context) {
+    return widget;
+  }), (route) => route == null);
+}
+
+/// push 跳转并替换当前页面
+Future<T> pushReplacement<T>({
+  @required BuildContext context,
+  rootNavigator = true,
+  Widget widget,
+}) {
+//  return Navigator.pushReplacement(context,
+//      MaterialPageRoute(builder: (context) {
+//        return widget;
+//      }));
+
+  return Navigator.of(context).pushReplacement(
+    MaterialPageRoute(builder: (context) {
+      return widget;
+    }),
+  );
+}
+
 /// pop
 /// [result]返回结果
 pop<T>(
