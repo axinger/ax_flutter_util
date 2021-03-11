7b2560046ed9a967e05c3eee0c94d482fd983249
axinger <axinger@outlook.com>
2021-03-11 11:16:56 +0800

    update…

--- lib/src/extension/string_extension.dart
+++ lib/src/extension/string_extension.dart
@@ -6,7 +6,7 @@ import 'reg_exp_extension.dart';
 extension StringExtension on String {
   /// 是否是电话号码
   bool get isMobileNumber {
-    if (this?.isNotEmpty != true) return false;
+    if (this.isNotEmpty != true) return false;
 //    return RegExp(r'^((13[0-9])|(14[5,7,9])|(15[^4])|(18[0-9])|(17[0,1,3,5,6,7,8])|(19)[0-9])\d{8}$').hasMatch(this);
     return RegExpExtension.phone.hasMatch(this);
   }
